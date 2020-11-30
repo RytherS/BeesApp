14e7953dbc7bb3037c0e05e381ab450273183d74
Sebastian Ryther <sryther@kimobility.com>
2020-11-29 18:07:37 -0600

    Initial commit

---
+++ BeesApp/ViewModels/HiveViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Bees.ClientData.Models;
+using Bees.SharedData.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeesApp.ViewModels
+{
+    public class HiveViewModel
+    {
+        public Hive Hive { get; set; }
+
+        public string HiveType { get => Enum.GetName(typeof(eHiveType), Hive.HiveType); }
+
+        public HiveViewModel()
+        {
+            this.Hive = new Hive();
+        }
+    }
+}
---
+++ BeesApp/Views/HiveView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using BeesApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BeesApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HiveView : ContentView
+    {
+        public HiveView()
+        {
+            InitializeComponent();
+            this.BindingContext = new HiveViewModel();
+        }
+    }
+}
